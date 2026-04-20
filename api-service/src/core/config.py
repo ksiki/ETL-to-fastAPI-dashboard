4a1b4888ea601c524ff544318d2c2bb9dc23aab2
ksiki <j.kolyasha@gmail.com>
2026-04-20 23:55:59 +0300

    add config - settings - prefixes

---
+++ api-service/src/core/config.py
@@ -0,0 +1,25 @@
+from typing import Final
+from decouple import config
+from pydantic_settings import BaseSettings
+
+
+class Settings(BaseSettings):
+    debug: bool
+    db_url: str
+
+
+class Prefixes(BaseSettings):
+    user: str
+    sales: str
+
+
+SETTINGS: Final[Settings] = Settings(
+    debug=config("DEBUG"),
+    db_url=config("DB_URL")
+)
+
+
+PREFIXES: Final[Prefixes] = Prefixes(
+    user="/user",
+    sales="/sales"
+)
